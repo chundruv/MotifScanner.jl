4245a634c056046b6ef5c33f99b6f29339644b10
Nick Owens <owensnick@gmail.com>
2026-03-03 12:33:38 +0000

    remove plots deps

--- src/MotifScanner.jl
+++ src/MotifScanner.jl
@@ -1,21 +1,22 @@
 module MotifScanner
 
 using BioSequences
-using Plots
 using StatsBase
 using DataFrames
-import PyPlot
+
 using FASTX
 using ProgressMeter
 
 ## This is a package for scanning motifs in DNA sequences and plotting sequence motifs.
 
+
+
 export loadmeme, loadmemelibrary, loadhomer, scanmotif, scanmotstats, consensus, plotseq, plotletter!, seqlogo!, seqlogo, loadrefseqs, motifscanall, loadtransfac
 
 include("loadmeme.jl")
 include("scanning.jl")
-include("plotseqlogo.jl")
-include("seqlogodata.jl")
+# include("plotseqlogo.jl")
+# include("seqlogodata.jl")
 
 include("variants.jl")
 end
